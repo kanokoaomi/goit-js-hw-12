10361e2cac72079c4ab68437f194ac2c2202a95b
kanokoaomi <skoroglyadliza@gmail.com>
2024-08-27 20:52:33 +0200

    Update main.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,1 +1,31 @@
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+export function createGallery(pictureInfo) {
+  return `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${pictureInfo.largeImageURL}">
+        <img 
+        class="gallery-image"
+        src="${pictureInfo.webformatURL}"
+        data-source="${pictureInfo.largeImageURL}"
+        alt="${pictureInfo.tags}" 
+        />
+        <ul class="picture-data">
+      <li class="picture-data-item"><p class="picture-data-item-text"><b>Likes:</b><span class="picture-data-item-span">${pictureInfo.likes}</span></p></li>
+      <li class="picture-data-item"><p class="picture-data-item-text"><b>Views:</b><span class="picture-data-item-span">${pictureInfo.views}</span></p></li>
+      <li class="picture-data-item"><p class="picture-data-item-text"><b>Comments:</b><span class="picture-data-item-span">${pictureInfo.comments}</span></p></li>
+      <li class="picture-data-item"><p class="picture-data-item-text"><b>Downloads:</b><span class="picture-data-item-span">${pictureInfo.downloads}</span></p></li>
+      </ul>
+      </a>
+    </li>
+    `;
+}
+
+export const lightbox = new simpleLightbox('.gallery-item a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export const listOfImg = document.querySelector('.gallery');
--- src/main.js
+++ src/main.js
@@ -2,5 +2,56 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { makeAFetch } from './pixabay-api.js';
-// import renderFunctions from './js/render-functions';
+
+import { fetchReturn, listOfImg, form } from './js/pixabay-api';
+import { createGallery, lightbox } from './js/render-functions';
+
+const onFormElement = pictureInfo => {
+  event.preventDefault();
+  const inputResult = form.elements.user_search.value.trim();
+
+  listOfImg.innerHTML = '';
+
+  showLoader();
+
+  fetchReturn(inputResult)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.show({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      } else {
+        const galleryCreating = data.hits
+          .map(imgInfo => createGallery(imgInfo))
+          .join('');
+        listOfImg.innerHTML = galleryCreating;
+        lightbox.refresh();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(() => {
+      hideLoader();
+    });
+
+  form.reset();
+};
+
+const loader = document.querySelector('.loader');
+const showLoader = () => {
+  loader.style.display = 'block';
+};
+const hideLoader = () => {
+  loader.style.display = 'none';
+};
+
+form.addEventListener('submit', onFormElement);
+
+listOfImg.addEventListener('click', event => {
+  if (event.target.classList.contains('gallery-image')) {
+    event.preventDefault();
+    lightbox.open();
+  }
+});
